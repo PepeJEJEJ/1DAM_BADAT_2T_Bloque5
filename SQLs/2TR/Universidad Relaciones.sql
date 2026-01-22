581f6f332f062368c243e4c3e770eab9d8edc975
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-22 10:40:03 +0100

    Update Universidad Relaciones.sql

--- SQLs/2TR/Universidad Relaciones.sql
+++ SQLs/2TR/Universidad Relaciones.sql
@@ -1,9 +1,9 @@
 USE `universidad`;
 
 -- 1 Obtener la alumna con mayor edad.
-select min(fecha_nacimiento) from persona where sexo='M' and tipo='alumno';
+select * from persona where fecha_nacimiento = (select min(fecha_nacimiento) where sexo='M' and tipo='alumno');
 -- 2 Mostrar los datos de la asignatura cuyo id sea el mayor.
-
+select * from asignatura where id = (select max(id) from asignatura);
 -- 3 Mostrar el nombre de la asignatura y el nombre de su grado asociado.
 
 -- 4 Mostrar el nombre de todas las asignaturas, el nombre de su grado, y el nombre y apellidos del profesor asociado (incluyendo asignaturas sin profesor si las hubiera).
