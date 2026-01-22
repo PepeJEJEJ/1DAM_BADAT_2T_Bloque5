c4ec869d61114cb5eb688d35977158e9741b6e0e
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-22 11:10:27 +0100

    Update Universidad Relaciones.sql

--- SQLs/2TR/Universidad Relaciones.sql
+++ SQLs/2TR/Universidad Relaciones.sql
@@ -5,8 +5,17 @@ select * from persona where fecha_nacimiento = (select min(fecha_nacimiento) whe
 -- 2 Mostrar los datos de la asignatura cuyo id sea el mayor.
 select * from asignatura where id = (select max(id) from asignatura);
 -- 3 Mostrar el nombre de la asignatura y el nombre de su grado asociado.
+select a.nombre, g.nombre from asignatura a join grado g on a.id_grado = g.id;
 
 -- 4 Mostrar el nombre de todas las asignaturas, el nombre de su grado, y el nombre y apellidos del profesor asociado (incluyendo asignaturas sin profesor si las hubiera).
+select a.nombre,g.nombre,p.nombre,p.apellido1,p.apellido2
+from asignatura a
+left join grado g on a.id_grado = g.id
+left join profesor pr on a.id_profesor = pr.id_profesor
+left join persona p on pr.id_profesor = p.id;
 
 -- 5 Mostrar el nombre y apellidos de los profesores y el nombre de su departamento.
-
+select pe.nombre, pe.apellido1,pe.apellido2,d.nombre
+from profesor pr
+join persona pe on pr.id_profesor = pe.id
+join departamento d on pr.id_departamento = d.id;
