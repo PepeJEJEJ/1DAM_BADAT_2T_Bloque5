c9f9a3df5413800a73d1a28a096f8fedb53f35db
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-22 09:42:56 +0100

    Create Universidad Relaciones.sql

---
+++ SQLs/2TR/Universidad Relaciones.sql
@@ -0,0 +1,12 @@
+USE `universidad`;
+
+-- 1 Obtener la alumna con mayor edad.
+
+-- 2 Mostrar los datos de la asignatura cuyo id sea el mayor.
+
+-- 3 Mostrar el nombre de la asignatura y el nombre de su grado asociado.
+
+-- 4 Mostrar el nombre de todas las asignaturas, el nombre de su grado, y el nombre y apellidos del profesor asociado (incluyendo asignaturas sin profesor si las hubiera).
+
+-- 5 Mostrar el nombre y apellidos de los profesores y el nombre de su departamento.
+
