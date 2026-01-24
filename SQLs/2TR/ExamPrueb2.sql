46b5c42688a8305e8495c6d623f8341c5cfb8298
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-24 12:06:04 +0100

    Create ExamPrueb2.sql

---
+++ SQLs/2TR/ExamPrueb2.sql
@@ -0,0 +1,49 @@
+USE `universidad`;
+
+select * from persona ;
+select * from asignatura ;
+select * from profesor ;
+select * from departamento ;
+select * from grado ;
+select * from curso_escolar;
+select * from alumno_se_matricula_asignatura;
+
+-- 1. Obtén el nombre y apellidos de la alumna más joven registrada en la tabla persona.
+select id, nif, nombre, apellido1, apellido2, ciudad, direccion, telefono, min(fecha_nacimiento), sexo, tipo from persona where sexo='M' and tipo='alumno';
+-- 2. Muestra el nombre y apellidos de los profesores que no tienen asignaturas asignadas.
+
+-- 3. Lista el nombre de cada asignatura junto con el número de alumnos matriculados en ella.
+
+-- 4. Muestra el nombre de las asignaturas y el nombre del grado al que pertenecen, pero solo aquellas que tengan más de 6 créditos.
+
+-- 5. Obtén el nombre completo del profesor y el nombre del departamento al que pertenece.
+
+-- 6. Muestra los grados que no tienen ninguna asignatura asociada.
+
+-- 7. Obtén el nombre y apellidos de cada alumno junto con el nombre de las asignaturas en las que está matriculado.
+
+-- 8. Muestra el nombre completo de los profesores y el número de asignaturas que imparten.
+
+-- 9. Lista el nombre de la asignatura, el nombre del grado y el nombre completo del profesor responsable.
+
+-- 10. Obtén los nombres y apellidos de los alumnos que se matricularon en el curso escolar 2018/2019.
+
+-- 11. Muestra todas las asignaturas que no tienen profesor asignado.
+
+-- 12. Obtén el nombre de los departamentos y el número de profesores que pertenecen a cada uno.
+
+-- 13. Lista los alumnos junto con el curso escolar en el que se matricularon y las asignaturas correspondientes.
+
+-- 14. Muestra el nombre de los grados y el número total de asignaturas que tienen asociadas.
+
+-- 15. Obtén el nombre y apellidos de los profesores que pertenecen al departamento de “Informática”.
+
+-- 16. Muestra el nombre de los alumnos y el número de asignaturas en las que están matriculados.
+
+-- 17. Obtén el nombre de las asignaturas optativas y el grado al que pertenecen.
+
+-- 18. Lista los profesores junto con el nombre de las asignaturas que imparten y el curso en el que se dan.
+
+-- 19. Muestra los alumnos matriculados en asignaturas del segundo cuatrimestre.
+
+-- 20. Obtén el nombre de los grados y el número de alumnos matriculados en sus asignaturas.
