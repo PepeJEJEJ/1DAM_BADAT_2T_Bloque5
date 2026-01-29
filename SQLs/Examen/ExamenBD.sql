a6d9143039b6f6fe9e369e9dbb878266fdf32c8d
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-29 10:07:02 +0100

    Update ExamenBD.sql

--- SQLs/Examen/ExamenBD.sql
+++ SQLs/Examen/ExamenBD.sql
@@ -1,1 +1,9 @@
 USE `universidad`;
+
+select * from persona ;
+select * from asignatura ;
+select * from profesor ;
+select * from departamento ;
+select * from grado ;
+select * from curso_escolar;
+select * from alumno_se_matricula_asignatura;
